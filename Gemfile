ea98714c7180412507014fe7357ebef427f9d05d
Zethus Suen <zethus@stackpop.com>
2013-09-13 23:38:18 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'bourbon' '~> 3.1.8'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jbuilder', '~> 1.2'
+gem 'mysql2'
+gem 'ng-rails-csrf' '~> 0.1.0'
+gem 'rails', '4.0.0'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+
+# gem 'turbolinks'
+
+
+# Use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Use Capistrano for deployment
+# gem 'capistrano', group: :development
+
+# Use debugger
+# gem 'debugger', group: [:development, :test]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsAngularSkeleton::Application.config.secret_key_base = '1b16fe04296d476642a6edd675e9eaa4b8ede09576c1ebd205e3fafa8c499c9dd2ea4cd5f77d16e32c2605bde72f0657766762d253dc8d389712c27c4282ba3f'
