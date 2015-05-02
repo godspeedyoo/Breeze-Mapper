7f8e82fefcf6c79e973eb952b3eeac89b2e24346
godspeedyoo <godspeedyoo@gmail.com>
2015-05-02 13:41:26 -0700

    add toggle button to show locations on home

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -1,8 +1,10 @@
 angular.module('myApp')
 .controller('HomeCtrl', ['locationService', '$scope', 
 	function (locationService, $scope) {
+    var locationToggle = false
 
     locationService.getLocations().success(function(response) {
     	$scope.locations = response;
     })
+
 }]);
