b1e2730739383aa372385bb078952eed2273fd74
godspeedyoo <godspeedyoo@gmail.com>
2015-05-04 15:52:02 -0700

    Finish refactoring getTransactions to be independent of transaction type; restructured 's chartData object to be more reusable; implement clearTransactions button - need to implement function

--- app/assets/javascripts/ng-app/controllers/chart-controller.js
+++ app/assets/javascripts/ng-app/controllers/chart-controller.js
@@ -2,10 +2,10 @@ angular.module('myApp')
 .controller('ChartCtrl', ['userService', 'locationService', 'transactionService', '$scope',
 	function (userService, locationService, transactionService, $scope) {
 
-  $scope.transactions = {};
   $scope.chartData = {};
-  $scope.chartData.earnings = [];
-  $scope.chartData.earningDates = [];
+  $scope.chartData.transactions = {};
+  $scope.chartData.data = [];
+  $scope.chartData.labels = [];
 
   locationService.getLocations().success(function(response) {
   	$scope.locations = response;
@@ -17,19 +17,19 @@ angular.module('myApp')
 
   $scope.getTransactions = function(transaction_type) {
     transactionService.getTransactions(transaction_type).success(function(response) {
-      $scope.transactions.earnings = response;
+      $scope.chartData.transactions = response;
 
-      $scope.transactions.earnings.forEach(function(el, i, arr) {
+      $scope.chartData.transactions.forEach(function(el, i, arr) {
         // returns suppressed labels due to lack of grouping
         // of data label points in chartjs library.
         // This allows for inefficient use of space in array elements 
         // that are used as spacers and can be concern in a large 
         // data set, however is a temporary workaround to enable clean labels.
-        $scope.chartData.earnings.push(el.amount);
+        $scope.chartData.data.push(el.amount);
         // store empty string to 'skip' (every 20) the label from being rendered.
-        if (i % 20 != 0) { $scope.chartData.earningDates.push("") }
+        if (i % 20 != 0) { $scope.chartData.labels.push("") }
         else { 
-          $scope.chartData.earningDates.push(
+          $scope.chartData.labels.push(
             // applies chart-ready formatting to dates
             el.created_at.split('T')[0].slice()
         )}
@@ -44,11 +44,9 @@ angular.module('myApp')
   
   // chart.js 
 
-  $scope.labels = $scope.chartData.earningDates;
+  $scope.labels = $scope.chartData.labels;
   $scope.series = ['Earned', 'Charged'];
-  $scope.data = [
-      $scope.chartData.earnings
-  ];
+  $scope.data = [ $scope.chartData.data ];
   $scope.onClick = function (points, evt) {
      console.log(points, evt);
   };
