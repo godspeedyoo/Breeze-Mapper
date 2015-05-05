a04afe98700dbd8ecadfd63900df39e2e15c5e88
godspeedyoo <godspeedyoo@gmail.com>
2015-05-05 13:01:26 -0700

    Fixed up routes to properly nest chart tabs for transactions. Need to wire up locations and users chart tabs

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -16,8 +16,12 @@ angular
                 '@': { templateUrl: 'home.html'},
                 'test@home': { templateUrl: 'test.html' },
                 'charts@home': { templateUrl: 'charts.html' }
-                
             }
+        })
+        .state('home.transactions', {
+            url: '/transactions',
+            templateUrl: 'charts.transactions.html',
+            controller: 'TransactionCtrl'
         })
         //     templateUrl: 'charts.html',
         //     views: {
@@ -45,6 +49,12 @@ angular
     // enable HTML5 Mode for SEO
     // $locationProvider.html5Mode(true);
 })
-.run(function($state) {
-    $state.go('home');
-});
+.run(['$rootScope', '$state', '$stateParams',
+      function ($rootScope, $state, $stateParams) {
+      // It's very handy to add references to $state and $stateParams to the $rootScope
+      // so that you can access them from any scope within your applications.For example,
+      // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+      // to active whenever 'contacts.list' or one of its decendents is active.
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+      }]);
