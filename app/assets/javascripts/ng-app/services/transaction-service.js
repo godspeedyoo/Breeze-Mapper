1302aa5150d8dfb25b4cb317b49851a151eb3fb3
godspeedyoo <godspeedyoo@gmail.com>
2015-05-04 21:45:21 -0700

    refactor transaction service for cleaner options in query string

--- app/assets/javascripts/ng-app/services/transaction-service.js
+++ app/assets/javascripts/ng-app/services/transaction-service.js
@@ -5,9 +5,13 @@ angular.module('myApp')
 	var transactions = {};
 
 	transactions.getTransactions = function(options) {
-		var userOption = options['user_id'] ? '?user_id=' + options['user_id'] : "";
-		var typeOption = options['type'] ? '?type=' + options['type'] : "";
-		return $http.get('/transactions' + userOption + typeOption);
+		var userOption = 'user_id=' + options['userId'] + '&';
+		var earningOption = 'earning=' + options['earning'] + '&';
+		var chargeOption = 'charge=' + options['charge'] + '&';
+		return $http.get('/transactions' + '?' + 
+											userOption +
+											earningOption +
+											chargeOption);
 	}
 
 	return transactions;
