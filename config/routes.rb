672a08091dd093ce84977ffc75d9dc945420978b
godspeedyoo <godspeedyoo@gmail.com>
2015-05-11 16:43:03 -0700

    cleanup unused files and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 RailsAngularSkeleton::Application.routes.draw do
   root 'application#index'
   get '/data', to: 'data#index'
-  get '/locations', to: 'location#index'
-  get '/users', to: 'user#index'
-  get '/transactions', to: 'transaction#index'
-  
   get '*path' => 'application#index'
-
-  
 end
