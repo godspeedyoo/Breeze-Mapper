8554ede0f6da7b7e8ab3a83952c6d2677ce138e4
godspeedyoo <godspeedyoo@gmail.com>
2015-05-11 16:59:11 -0700

    add rails routes back in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 RailsAngularSkeleton::Application.routes.draw do
   root 'application#index'
   get '/data', to: 'data#index'
+  get '/locations', to: 'location#index'
+  get '/users', to: 'user#index'
+  get '/transactions', to: 'transaction#index'
+  
   get '*path' => 'application#index'
+
+  
 end
