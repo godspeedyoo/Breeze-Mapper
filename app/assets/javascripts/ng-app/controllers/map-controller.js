b460dbe3ba370ba3128fe338cd4057f0bc0f8187
godspeedyoo <godspeedyoo@gmail.com>
2015-05-11 15:15:22 -0700

    Clean up map controller code and add comments

--- app/assets/javascripts/ng-app/controllers/map-controller.js
+++ app/assets/javascripts/ng-app/controllers/map-controller.js
@@ -4,9 +4,17 @@ angular.module('myApp')
     var map;
     var markers = [];
     var dataService = dataService;
-    
-    // this allows the map instance from link to be passed into the
-    // controller
+    var infoWindow;    
+
+    // listen for generic submit button and do everything marker/map related.
+    $rootScope.$on('submit', function (scope, userId) {
+      $scope.getMapData(userId);
+      $scope.renderMarkers();
+      $scope.setZoom();
+    })
+
+    // this.registerMap allows the map instance from link (in the directive) 
+    // to be passed into the controller.
     this.registerMap = function (myMap) {
       var center = myMap.getCenter(),
         latitude = center.lat(),
@@ -14,32 +22,59 @@ angular.module('myApp')
       map = myMap;
       $scope.latitude = latitude;
       $scope.longitude = longitude;
+
     };
 
     // populates an array of LatLng objects
-    $scope.getLocations = function (userId) {
+    $scope.getMapData = function (userId) {
       $scope.locations = [];
+      $scope.dates = [];
+
       var locations = dataService.data[userId].locations;
       for (loc in locations) {
+        // store coordinates data to $scope
         $scope.locations.push(new google.maps.LatLng(
           locations[loc].latitude, 
           locations[loc].longitude));
+        // store tooltip data - in this case it is only the created_at field
+        $scope.dates.push(locations[loc].created_at.substring(0,10));
       }
     }
 
     $scope.renderMarkers = function () {
       $scope.clearMarkers();
       for (var i = 0; i < $scope.locations.length; i++) {
-        $scope.addMarker($scope.locations[i]);
+        $scope.addMarker($scope.locations[i], i);
       }
     }
 
-    $scope.addMarker = function (position) {
-      markers.push(new google.maps.Marker({
+
+    // index is passed in addMarker() and addTooltip() because the data which
+    // they rely upon is stored in separate arrays $scope.locations and $scope.dates.
+    // It is easier to deal with single arrays of relevant data, but 
+    // this may cause issues if $scope.locations and $scope.dates need to
+    // be reordered - I don't see a case for this here, however.
+    $scope.addMarker = function (position, index) {
+      var marker = new google.maps.Marker({
         position: position,
         map: map,
         animation: google.maps.Animation.DROP
-      }));
+      });
+      $scope.addToolTip(marker, index);
+      markers.push(marker);
+    }
+
+    $scope.addToolTip = function (marker, index) {
+      // index is used here to reference the index at which tooltip data is stored
+      var infoWindow = new google.maps.InfoWindow({content: $scope.dates[index] });
+      // mouse over open tooltip
+      google.maps.event.addListener(marker, 'mouseover', function () {
+        infoWindow.open(map, marker);
+      });
+      // mouse out close tooltip
+      google.maps.event.addListener(marker, 'mouseout', function () {
+        infoWindow.close();
+      });
     }
 
     $scope.clearMarkers = function () {
@@ -57,9 +92,6 @@ angular.module('myApp')
       }
     }
 
-    $rootScope.$on('submit', function (scope, userId) {
-      $scope.getLocations(userId);
-      $scope.renderMarkers();
-      $scope.setZoom();
-    })
+
+
 }]);
