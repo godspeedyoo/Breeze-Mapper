89d9ad0bdab1ceb849031a1448cef10e7ae13d42
godspeedyoo <godspeedyoo@gmail.com>
2015-05-10 18:10:24 -0700

    fix controller inection error

--- app/assets/javascripts/ng-app/directives/google-maps.js
+++ app/assets/javascripts/ng-app/directives/google-maps.js
@@ -1,7 +1,7 @@
 angular.module('myApp')
 .directive('googleMaps', [function () {
   return {
-    controller: ['MapCtrl'],
+    controller: 'MapCtrl',
 
     link: function (scope, elem, attrs, ctrl) {
       var mapOptions,
